d76d952a63458236d226ac549de27797d2bfc3b3
Roy Bar-On <royb997@gmail.com>
2023-09-12 01:48:15 +0300

    Started expanding object matrix

--- src/RTL/VGA/board/object_matrix.sv
+++ src/RTL/VGA/board/object_matrix.sv
@@ -8,11 +8,14 @@ module object_matrix (
     input logic collision, // indicates if there is a collision with the player on the current pixel
     input logic explosion, // Indicates if there is an explosion in the current pixel
     input logic add_mine,
+    input logic add_user_bomb,
+    input logic is_player_location,
 
     output logic [10:0] tile_x, // top-left pixel of selected tile
     output logic [10:0] tile_y,
     output logic mine_exploded,
-    output logic [2:0] object, // indicator for object in selected tile
+	 output logic game_won,
+    output logic [3:0] object, // indicator for object in selected tile
     output reg [3:0] mine_count
 );
 
@@ -20,23 +23,29 @@ parameter logic [7:0] TRANSPARENT_COLOUR = 8'hFF;
 
 parameter logic [10:0] X_MATRIX = 11'h020; // Distance from screen left.
 parameter logic [10:0] Y_MATRIX = 11'h060; // Distance from screen top.
-
+localparam [4:0] ROWS = 11;
+localparam [4:0] COLUMNS = 17;
 parameter logic [3:0] tile_order = 5; // Tile size = 2**order
-
-parameter logic [3:0] max_mines = 10;
 
 /* Different values for different objects:
     0: Background
     1: Persistent wall
     2: Sturdy wall
     3: Brittle wall
-    4: Mine
+    4: Added mine
+    5: Static mine
+    6: Time bomb
+    7: Time booster
+    8: User bomb
+    a: Sturdy explosion
+    b: Brittle explosion
 */
+logic [0:ROWS - 1][0:COLUMNS - 1] [3:0] objects;
 
-localparam [4:0] ROWS = 11;
-localparam [4:0] COLUMNS = 17;
+parameter logic [3:0] max_mines = 10;
+parameter logic [3:0] max_bombs = 1;
 
-logic [0:ROWS - 1][0:COLUMNS - 1] [2:0] objects;
+logic [3:0] bomb_count = 4'h0;
 
 logic collision_prev = 1'b0;
 logic collision_cur = 1'b0;
@@ -53,42 +62,70 @@ logic on_board;
 always_ff @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
         mine_exploded <= 1'b0;
+        game_won <= 1'b0;
 
         collision_prev <= 1'b0;
         explosion_prev <= 1'b0;
-        mine_count <= 4'h1;
+        bomb_count <= 4'h0;
+        mine_count <= 4'h0;
 
         objects <= {
-            {3'h0, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0},
-            {3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h2, 3'h1, 3'h2, 3'h1, 3'h0},
-            {3'h0, 3'h2, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h2, 3'h0},
-            {3'h0, 3'h1, 3'h3, 3'h1, 3'h0, 3'h1, 3'h2, 3'h1, 3'h2, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h2, 3'h1, 3'h0},
-            {3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h3, 3'h0, 3'h0, 3'h0, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0},
-            {3'h0, 3'h1, 3'h2, 3'h1, 3'h2, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0},
-            {3'h0, 3'h0, 3'h2, 3'h3, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h2, 3'h0, 3'h0, 3'h0, 3'h2, 3'h3, 3'h3, 3'h0},
-            {3'h0, 3'h1, 3'h2, 3'h1, 3'h2, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0},
-            {3'h0, 3'h0, 3'h4, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h3, 3'h0, 3'h0, 3'h0},
-            {3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h0, 3'h1, 3'h2, 3'h1, 3'h2, 3'h1, 3'h0},
-            {3'h0, 3'h0, 3'h0, 3'h2, 3'h3, 3'h2, 3'h2, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0, 3'h0},
+            {4'h0, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h7},
+            {4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h2, 4'h1, 4'h2, 4'h1, 4'h0},
+            {4'h0, 4'h2, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h2, 4'h0},
+            {4'h0, 4'h1, 4'h3, 4'h1, 4'h0, 4'h1, 4'h2, 4'h1, 4'h2, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h2, 4'h1, 4'h0},
+            {4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h3, 4'h0, 4'h0, 4'h0, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0},
+            {4'h0, 4'h1, 4'h2, 4'h1, 4'h2, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0},
+            {4'h0, 4'h0, 4'h2, 4'h3, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h2, 4'h0, 4'h0, 4'h0, 4'h2, 4'h3, 4'h3, 4'h0},
+            {4'h0, 4'h1, 4'h2, 4'h1, 4'h2, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0},
+            {4'h0, 4'h0, 4'h5, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h3, 4'h3},
+            {4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h0, 4'h1, 4'h2, 4'h1, 4'h3, 4'h1, 4'h2},
+            {4'h7, 4'h0, 4'h0, 4'h2, 4'h3, 4'h2, 4'h2, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h0, 4'h3, 4'h2, 4'h6},
         };
     end
     else begin
         if (on_board) begin
             case (objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order])
-                3'h0: if (add_mine_up && (mine_count < max_mines)) begin
-                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 3'h4;
+                4'h0: begin
+                    if (add_mine_up && (mine_count < max_mines)) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h4;
                         mine_count <= mine_count + 1;
+                    end else if (add_user_bomb && is_player_location && (bomb_count < max_bombs)) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h8;
+                    end
                 end
-                3'h2: if (explosion_up) begin
-                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 3'h3;
+                4'h2: begin
+                    if (explosion_up) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h3;
+                    end
                 end
-                3'h3: if (explosion_up) begin
-                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 3'h0;
+                4'h3: begin
+                    if (explosion_up) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h0;
+                    end
                 end
-                3'h4: if (collision_up) begin
-                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 3'h0;
+                4'h4: begin
+                    if (collision_up) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h0;
                         mine_count <= mine_count - 1;
-                        mine_exploded = 1'b1;
+                        mine_exploded <= 1'b1;
+                    end else if (explosion) begin
+                        objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h0;
+                        mine_count <= mine_count - 1;
+                    end
+                end
+                4'h5: begin
+                    if (collision_up) begin
+                            objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h0;
+                            mine_exploded <= 1'b1;
+                    end else if (explosion) begin
+                            objects[(pixel_y - Y_MATRIX) >> tile_order][(pixel_x - X_MATRIX) >> tile_order] <= 4'h0;
+                    end
+                end
+                4'h6: begin
+                    if (collision_up) begin
+                        game_won <= 1'b1;
+                    end
                 end
             endcase
         end
@@ -114,7 +151,7 @@ always_comb begin
     if (!on_board) begin
         tile_x = 11'h0;
         tile_y = 11'h0;
-        object = 3'h0;
+        object = 4'h0;
     end
     else begin
         tile_x = pixel_x & 11'h7E0;
