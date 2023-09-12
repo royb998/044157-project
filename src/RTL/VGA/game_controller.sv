4c6d6bb046888aecf09f4735af8ed41aa0265bf7
Roy Bar-On <royb997@gmail.com>
2023-09-13 01:24:17 +0300

    Some top-level scheme cleanup

--- src/RTL/VGA/game_controller.sv
+++ src/RTL/VGA/game_controller.sv
@@ -4,56 +4,38 @@
 //updated --Eyal Lev 2021
 
 
-module	game_controller	(
-			input logic clk,
-			input logic resetN,
-			input logic startOfFrame,  // short pulse every start of frame 30Hz
-			input logic drawing_request_Ball,
-			input logic drawing_request_1,
-			input logic drawing_request_2,
+module game_controller (
+    input logic clk,
+    input logic resetN,
+    input logic startOfFrame,  // short pulse every start of frame 30Hz
+    input logic draw_request_player,
+    input logic draw_request_object,
+    input logic draw_request_bg,
 
-			output logic collision, // active in case of collision between two objects
-			output logic SingleHitPulse // critical code, generating A single pulse in a frame
-);
+    output logic collision, // active in case of collision between two objects
+    output logic SingleHitPulse // critical code, generating A single pulse in a frame
+    );
 
-// drawing_request_Ball   -->  smiley
-// drawing_request_1      -->  brackets
-// drawing_request_2      -->  number/box
-
-
-// add colision between number and Smiley
-//_______________________________________________________
-
-assign collision_smiley_number = drawing_request_Ball && drawing_request_2;
-
-assign collision = (drawing_request_Ball && drawing_request_1) || collision_smiley_number;// any collision
-
-
-
+assign collision = draw_request_player && (draw_request_object || draw_request_bg);
 
 logic flag ; // a semaphore to set the output only once per frame / regardless of the number of collisions
 
-always_ff@(posedge clk or negedge resetN)
-begin
-	if(!resetN)
-	begin
-		flag	<= 1'b0;
-		SingleHitPulse <= 1'b0 ;
-	end
-	else begin
+always_ff @(posedge clk or negedge resetN) begin
+    if (!resetN) begin
+        flag <= 1'b0;
+        SingleHitPulse <= 1'b0;
+    end else begin
+        SingleHitPulse <= 1'b0;
 
-			SingleHitPulse <= 1'b0 ; // default
-			if(startOfFrame)
-				flag <= 1'b0 ; // reset for next time
+        if(startOfFrame) begin
+            flag <= 1'b0 ; // reset for next time
+        end
 
-//		change the section below  to collision between number and smiley
-
-
-if ( collision_smiley_number && (flag == 1'b0)) begin
-			flag	<= 1'b1; // to enter only once
-			SingleHitPulse <= 1'b1 ;
-		end ;
-	end
+        if (collision && (flag == 1'b0)) begin
+            flag <= 1'b1; // to enter only once
+            SingleHitPulse <= 1'b1 ;
+        end
+    end
 end
 
 endmodule
